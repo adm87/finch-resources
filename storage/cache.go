98c82e01f6437b3f9be62f3f60fb0589baf5e2bc
Aaron Malley <aaron.d.malley@gmail.com>
2025-08-01 12:46:57 -0300

    removed redundant struct store[T] is now a typed map, removed pointer receivers and fixed associated errors. resource cache now specifies stores store points to loaded resource instead of values

--- storage/cache.go
+++ storage/cache.go
@@ -59,8 +59,8 @@ type ResourceCache struct {
 	manifest    manifest.ResourceManifest
 	filesystems map[string]fs.FS
 
-	imageStore Store[ebiten.Image]
-	dataStore  Store[[]byte]
+	imageStore Store[*ebiten.Image]
+	dataStore  Store[*[]byte]
 }
 
 // NewResourceCache creates a new ResourceCache instance.
@@ -68,12 +68,8 @@ func NewResourceCache() *ResourceCache {
 	return &ResourceCache{
 		manifest:    manifest.ResourceManifest{},
 		filesystems: make(map[string]fs.FS),
-		imageStore: Store[ebiten.Image]{
-			items: make(map[string]*ebiten.Image),
-		},
-		dataStore: Store[[]byte]{
-			items: make(map[string]*[]byte),
-		},
+		imageStore:  make(Store[*ebiten.Image]),
+		dataStore:   make(Store[*[]byte]),
 	}
 }
 
@@ -123,14 +119,14 @@ func (c *ResourceCache) RemoveFilesystem(root string) error {
 // Images returns the image store of the cache.
 //
 // This store contains loaded images. Call LoadImage to load images into this store.
-func (c *ResourceCache) Images() *Store[ebiten.Image] {
+func (c *ResourceCache) Images() *Store[*ebiten.Image] {
 	return &c.imageStore
 }
 
 // Data returns the data store of the cache.
 //
 // This store contains loaded data. Call LoadData to load data into this store.
-func (c *ResourceCache) Data() *Store[[]byte] {
+func (c *ResourceCache) Data() *Store[*[]byte] {
 	return &c.dataStore
 }
 
@@ -138,14 +134,14 @@ func (c *ResourceCache) Data() *Store[[]byte] {
 //
 // Resources are deallocated and removed from the cache, making them unusable.
 func (c *ResourceCache) ClearCache() {
-	for name := range c.imageStore.items {
-		c.imageStore.items[name].Deallocate()
+	for name := range c.imageStore {
+		c.imageStore[name].Deallocate()
 	}
-	for name := range c.dataStore.items {
-		c.dataStore.items[name] = nil
+	for name := range c.dataStore {
+		c.dataStore[name] = nil
 	}
-	c.imageStore.items = make(map[string]*ebiten.Image)
-	c.dataStore.items = make(map[string]*[]byte)
+	c.imageStore = make(Store[*ebiten.Image])
+	c.dataStore = make(Store[*[]byte])
 }
 
 // Load loads resources by their names into the cache.
@@ -205,13 +201,12 @@ func (c *ResourceCache) Unload(names ...string) error {
 	}
 
 	for _, name := range linq.Distinct(names) {
-		if image, ok := c.imageStore.items[name]; ok {
+		if image, ok := c.imageStore[name]; ok {
 			image.Deallocate()
 			c.imageStore.Remove(name)
 			continue
 		}
-		if _, ok := c.dataStore.items[name]; ok {
-			c.dataStore.items[name] = nil
+		if _, ok := c.dataStore[name]; ok {
 			c.dataStore.Remove(name)
 			continue
 		}
--- storage/store.go
+++ storage/store.go
@@ -3,17 +3,11 @@ package storage
 import "github.com/adm87/finch-core/errors"
 
 // Store is a generic store for items of type T, identified by a string name.
-type Store[T any] struct {
-	items map[string]*T
-}
+type Store[T any] map[string]T
 
 // Add adds an item to the store with the given name.
 // Returns an error if the name is empty, the item is nil, or an item with the same name already exists.
-func (s *Store[T]) Add(name string, item *T) error {
-	if item == nil {
-		return errors.NewNilError("item cannot be nil")
-	}
-
+func (s Store[T]) Add(name string, item T) error {
 	hasItem, err := s.Has(name)
 	if err != nil {
 		return err
@@ -22,43 +16,45 @@ func (s *Store[T]) Add(name string, item *T) error {
 		return errors.NewDuplicateError("item already exists: " + name)
 	}
 
-	s.items[name] = item
+	s[name] = item
 	return nil
 }
 
 // Remove removes the item with the given name from the store.
 // Returns an error if the name is empty or if the item does not exist.
-func (s *Store[T]) Remove(name string) error {
+func (s Store[T]) Remove(name string) error {
 	if name == "" {
 		return errors.InvalidArgumentError("item name cannot be empty")
 	}
-	if _, exists := s.items[name]; !exists {
+	if _, exists := s[name]; !exists {
 		return errors.NewNotFoundError("item not found: " + name)
 	}
 
-	delete(s.items, name)
+	delete(s, name)
 	return nil
 }
 
 // Get retrieves the item with the given name from the store.
 // Returns an error if the name is empty or if the item does not exist.
-func (s *Store[T]) Get(name string) (*T, error) {
+func (s Store[T]) Get(name string) (T, error) {
 	hasItem, err := s.Has(name)
 	if err != nil {
-		return nil, err
+		var zero T
+		return zero, err
 	}
 	if !hasItem {
-		return nil, errors.NewNotFoundError("item not found: " + name)
+		var zero T
+		return zero, errors.NewNotFoundError("item not found: " + name)
 	}
-	return s.items[name], nil
+	return s[name], nil
 }
 
 // Has checks if an item with the given name exists in the store.
 // Returns an error if the name is empty.
-func (s *Store[T]) Has(name string) (bool, error) {
+func (s Store[T]) Has(name string) (bool, error) {
 	if name == "" {
 		return false, errors.InvalidArgumentError("item name cannot be empty")
 	}
-	_, exists := s.items[name]
+	_, exists := s[name]
 	return exists, nil
 }
